1b369530221506fbbb063ea918a2fcf98093fe8a
Danilo <95001485+D4N23@users.noreply.github.com>
2023-06-12 11:26:31 -0300

    Implementação de controller nova e view nova categori

--- resources/views/categorias.blade.php
+++ resources/views/categorias.blade.php
@@ -1,5 +1,36 @@
 @extends('layout.app', ["current" => "categorias"])
 
 @section('body')
-    <h4>Pagina de Categorias</h4>
+    <div class="card border">
+        <div class="card-title">
+            <h5 class="table table-ordered table-hover"> </h5>
+    @if(count($cats) > 0)
+                <table class="table table-ordered table-hover">
+                    <thead>
+                        <tr>
+                            <tr>Código</tr>
+                            <tr>Nome da Categoria</tr>
+                            <tr>Ações</tr>
+                        </tr>
+                    </thead>
+                    <tbody>
+            @foreach($cats as $cat)
+                        <tr>
+                            <td>{{$cat->id}}</td>
+                            <td>{{$cat->nome}}</td>
+                            <td>
+                                <a href="/categorias/editar/{{$cat->id}}" class="btn btn-sm btn-primary">Editar</a>
+                                <a href="/categorias/apagar/{{$cat->id}}" class="btn btn-sm btn-danger">Aoagar</a>
+                            </td>
+                        </tr>
+            @endforeach            
+                    </tbody>
+                </table>
+    @endif            
+        </div>
+        <div class="card-footer">
+            <a href="/cattegorias/novo" class="btn btn-sm btn-primary" role="button">Nova categoria</a>
+        </div>
+    </div>
+    
 @endsection
