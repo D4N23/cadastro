294efb83f3446ebe42c68902b65aca6af0309b9a
Danilo <95001485+D4N23@users.noreply.github.com>
2023-06-11 22:17:42 -0300

    Instalaçao e configuração do vite e do bootstrap

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta href="{{asset('css/app.css')}}" rel="stylesheet">
     <meta name="csrf_token" content="{{csrf_token()}}">
     <title>Cadastro de Produtos</title>
+    @vite(['resources/sass/app.scss','resources/js/app.js'])
 </head>
 <body>
     <div class="container">
@@ -14,6 +14,6 @@
             @endif
         </main>
     </div>
-    <script src="{{asset('js/app.js')}}" type="test/javascript"></script>
+    <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
 </body>
 </html>
